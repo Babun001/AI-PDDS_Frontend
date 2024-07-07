db94c8105e77f865043a6e11dd039fae61aa91b6
Babun001 <babunroy987@gmail.com>
2024-07-07 18:29:49 +0530

    ...

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,65 +1,61 @@
 import React from 'react';
-import { motion } from "framer-motion"
+// import { motion } from "framer-motion"
 
-// import { FaHome } from "react-icons/fa"
-import { FaUserDoctor } from "react-icons/fa6";
-import { TbCheckupList } from "react-icons/tb";
-import { BsInfoCircleFill } from "react-icons/bs";
-import { IoIosPeople } from "react-icons/io";
-import { IoCall } from "react-icons/io5";
+// // import { FaHome } from "react-icons/fa"
+// import { FaUserDoctor } from "react-icons/fa6";
+// import { TbCheckupList } from "react-icons/tb";
+// import { BsInfoCircleFill } from "react-icons/bs";
+// import { IoIosPeople } from "react-icons/io";
+// import { IoCall } from "react-icons/io5";
 
 
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 // function clicked (){
 //     alert(`You clicked on the button`);
 //     document.getElementsByClassName('btn')
 // }
 
-const Routes = [
-    {
-        path: "/",
-        Name: "Home",
-        icon: <FaUserDoctor />
-    },
-    {
-        path: "/Service",
-        Name: "Service",
-        icon: <TbCheckupList />
-    },
-    {
-        path: "/AboutUs",
-        Name: "About Us",
-        icon: <IoIosPeople />
-    },
-    {
-        path: "/path",
-        Name : "info.",
-        icon : <BsInfoCircleFill/>
-    },
-    {
-        path: "/ContactUs",
-        Name: "Contact With Us",
-        icon: <IoCall />
-    }
-]
+
 
 export default function Navbar({ child }) {
     return (
         <>
-            <div className='main-container'>
-
-                <motion.div animate={{ width: "250px" }} className='sidebar'>
-                    <div className='pages'>{Routes.map((rt) => (
-                        <NavLink to={Route.path} key={Route.Name}>
-                            <div className='icons'>{Route.icon}</div>
-                            <div className='page_link'>{Route.Name}</div>
-                        </NavLink>
-                    ))}</div>
-                </motion.div>
-                <main>
-                    {child}
-                </main>
-            </div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import video01 from "../Assets/Doctor01.mp4"
 
 // import Navbar from '../Components/Navbar';
 
 export default function Home() {
   return (
     <>
-      <div className='Home-main'>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat expedita consequatur voluptas quos necessitatibus nesciunt facilis nobis id omnis? Vero ipsa repellat corporis recusandae impedit mollitia laudantium eos suscipit? Excepturi.</h1>
+      <div className="bg-video">
+        <video src={video01} autoPlay muted loop/>
       </div>
-      
+
+      <div className="bg-text">
+        <h2>AIPDDS</h2>
+        <h1 style={{fontSize:"50px"}}>We are here only for you!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor illum, eaque hic delectus vel beatae tenetur quisquam praesentium ipsa eligendi ab veritatis odit molestias, blanditiis dignissimos consequatur id quis sed.</p>
+      </div>
     </>
   );
 }
