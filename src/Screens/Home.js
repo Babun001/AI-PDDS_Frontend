4fb4531cb76b9e13d27a6e8104ce6c6268ff930a
Babun001 <babunroy987@gmail.com>
2024-07-07 23:41:18 +0530

    add styling

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import video01 from "../Assets/Doctor01.mp4"
+// import video02 from "../Assets/Doctor02.mp4"
+// import video03 from "../Assets/Doctor03.mp4"
 
 // import Navbar from '../Components/Navbar';
 
@@ -7,13 +9,15 @@ export default function Home() {
   return (
     <>
       <div className="bg-video">
-        <video src={video01} autoPlay muted loop/>
+        <video className='video-clip' src={video01} autoPlay muted loop />
+      </div>
+      <div className="sitelogo">
+        <h2>AI-PDDS</h2>
       </div>
 
       <div className="bg-text">
-        <h2>AIPDDS</h2>
-        <h1 style={{fontSize:"50px"}}>We are here only for you!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor illum, eaque hic delectus vel beatae tenetur quisquam praesentium ipsa eligendi ab veritatis odit molestias, blanditiis dignissimos consequatur id quis sed.</p>
+        <h1 style={{ fontSize: "70px", marginBottom: "2%", color: "black", fontWeight: "bold" }}>We are here only for you!</h1>
+        <p>Welcome to AI PDDS, the cutting-edge platform revolutionizing disease diagnosis with the power of artificial intelligence. Our platform leverages advanced machine-learning algorithms and vast medical datasets to provide precise, swift and reliable disease diagnoses.</p>
       </div>
     </>
   );
