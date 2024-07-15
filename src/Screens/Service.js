07edb5eb7c20b6fea507496f5189e4d6e554ef27
Babun001 <babunroy987@gmail.com>
2024-07-15 22:34:23 +0530

    service page navigation

--- src/Screens/Service.js
+++ src/Screens/Service.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+
 // import Diabetes from "../Screens/Disease Template/Diabetes";
 import './Pages.css'
 // import ser_bg from '../Assets/neuron.jpeg';
@@ -7,8 +9,42 @@ import './Pages.css'
 export default function Service() {
   const navigate = useNavigate();
 
-  const Disease [
-    
+  const Disease = [
+    {
+      D_name: "Diabetes Mellitus",
+      D_img: '',
+      nav: '/Diabetes'
+    },
+
+    {
+      D_name: "Kidney Disease",
+      D_img: '',
+      nav: '/Kidney'
+    },
+
+    {
+      D_name: "Liver",
+      D_img: '',
+      nav: '/Liver'
+    },
+
+    {
+      D_name: "Parkinson Diasease",
+      D_img: '',
+      nav: '/ParkinsonDisease'
+    },
+
+    {
+      D_name: "Ratina",
+      D_img: '',
+      nav: '/Ratina'
+    },
+
+    {
+      D_name: "Skin Cancer",
+      D_img: '',
+      nav: '/SkinCancer'
+    }
   ]
 
   return (
@@ -17,12 +53,18 @@ export default function Service() {
         {/* <img src={ser_bg}/> */}
         <div className="service">
           <div className="allbtns">
-            <button className='btn btn-success m-2' onClick={() => navigate('/Diabetes')}>Diabetes</button>
-            <button className='btn btn-success m-2' onClick={() => navigate('/Kidney')}>Kidney</button>
-            <button className='btn btn-success m-2' onClick={() => navigate('/Liver')}>Liver</button>
-            <button className='btn btn-success m-2' onClick={() => navigate('/Ratina')}>Ratina</button>
-            <button className='btn btn-success m-2' onClick={() => navigate('/SkinCancer')}>Skin Cancer</button>
-            <button className='btn btn-success m-2' onClick={() => navigate('/ParkinsonDisease')}>Parkinson Disease</button>
+           
+
+            {Disease.map((route) => (
+              <NavLink className="DiaseaseLink" to={route.nav} key={route.D_nam}>
+                {/* <div className="icons">
+                  {route.icon}
+                </div> */}
+                <button className='btn btn-success m-2' onClick={() => navigate(route.nav)}>{route.D_name}</button>
+              </NavLink>
+            ))}
+
+
           </div>
         </div>
       </div>
