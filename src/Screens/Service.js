5e6d917765c65bd883c8db83bfc4a45b31adce6e
Babun001 <babunroy987@gmail.com>
2024-07-17 14:13:27 +0530

    add images

--- src/Screens/Service.js
+++ src/Screens/Service.js
@@ -1,69 +1,85 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { NavLink } from 'react-router-dom';
 
-// import Diabetes from "../Screens/Disease Template/Diabetes";
 import './Pages.css'
-// import ser_bg from '../Assets/neuron.jpeg';
+import diabetes_img from "../Assets/Diabetes.jpg";
+import kidney_img from "../Assets/kidney02.jpg";
+import Liver_img from "../Assets/Liver.jpg";
+import Parkinson_img from "../Assets/Parkinson.jpg";
+import ratina_img from "../Assets/Ratina.jpg";
+import skin_img from "../Assets/skin.jpeg";
 
 export default function Service() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const Disease = [
     {
       D_name: "Diabetes Mellitus",
-      D_img: '',
-      nav: '/Diabetes'
+      D_img: diabetes_img,
+      nav: '/Diabetes',
+      id : '01'
     },
 
     {
       D_name: "Kidney Disease",
-      D_img: '',
-      nav: '/Kidney'
+      D_img: kidney_img,
+      nav: '/Kidney',
+      id : '02'
     },
 
     {
       D_name: "Liver",
-      D_img: '',
-      nav: '/Liver'
+      D_img: Liver_img,
+      nav: '/Liver',
+      id : '03'
     },
 
     {
       D_name: "Parkinson Diasease",
-      D_img: '',
-      nav: '/ParkinsonDisease'
+      D_img: Parkinson_img,
+      nav: '/ParkinsonDisease',
+      id : '04'
     },
 
     {
       D_name: "Ratina",
-      D_img: '',
-      nav: '/Ratina'
+      D_img: ratina_img,
+      nav: '/Ratina',
+      id : '05'
     },
 
     {
       D_name: "Skin Cancer",
-      D_img: '',
-      nav: '/SkinCancer'
+      D_img: skin_img,
+      nav: '/SkinCancer',
+      id : '06'
     }
   ]
+
 
   return (
     <>
       <div className="serviceBG">
-        {/* <img src={ser_bg}/> */}
+
         <div className="service">
           <div className="allbtns">
-           
 
-            {Disease.map((route) => (
-              <NavLink className="DiaseaseLink" to={route.nav} key={route.D_nam}>
-                {/* <div className="icons">
-                  {route.icon}
-                </div> */}
-                <button className='btn btn-success m-2' onClick={() => navigate(route.nav)}>{route.D_name}</button>
+
+            {Disease.map((route) =>(
+              <NavLink className="DiaseaseLink" to={route.nav} key={route.id}>
+
+
+                <div className="card" >
+                  <img src={route.D_img} className="card-img-top" alt='Error'/>
+                    <div className="card-body">
+                      <h5 className="text">{route.D_name}</h5>
+                    </div>
+                </div>
+
+
               </NavLink>
             ))}
-
 
           </div>
         </div>
@@ -71,5 +87,3 @@ export default function Service() {
     </>
   );
 }
-
-// /Liver /Ratina  /SkinCancer  /ParkinsonDisease
